3c585ef4fa51d5d95e2380f44ac1026bee976818
MacKur <maciej.kuraz@gmail.com>
2020-12-23 15:17:45 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
+"""
+ALGORYTMY GENETYCZNE - ZADANIE PCHANIA WÓZKA
+
+Marek		Mikulski	165745
+Maciej	    Kuraż		165737
+"""
+
 import time
 import numpy as np
 import matplotlib.pyplot as plt
 import random
 
-class bcolors:
+
+class BColors:
     HEADER = '\033[95m'
     OKBLUE = '\033[94m'
     OKCYAN = '\033[96m'
@@ -15,63 +23,75 @@ class bcolors:
     UNDERLINE = '\033[4m'
 
 
-
+####################################################################################################
+#                                      Funkcja przystosowania                                      #
+# Dla każdego osobnika całej populacji (70 osobników rodzicielskich oraz 70 osobników potomnych    #
+# (zmutowanych)) obliczamy funkcję przystosowania wartość współczynnika J po ówczesnym odkodowaniu #
+####################################################################################################
 def fitness_function(_N, population):
     all_fitness = []
     u = []
     arr = []
-    x1 = 0
-    x2 = 0
-    sum_u = 0
-    k = 0
-    # print('len population', len(population))
-    for osobnik in population:
-        a = 0
-        b = 20
-        arr.clear()
-        u.clear()
-        # print('osobnik', osobnik)
-        osobnik = osobnik * values
-        # print('osobnik x values', osobnik)
-        for j in range(N):
-            arr.append(osobnik[a:b])
-            a = a + 20
-            b = b + 20
-            # v1 u.append(round(np.sum(arr[j]), 3))
-            u.append(np.sum(arr[j]))
-        #print('arr', arr)
-        # print('u', u)
-        for k in range(0, _N):
-            x1_new = x2
-            # v1 x2_new = round(2 * x2 - x1 + 1 / (N ** 2) * u[k], 3)
-            x2_new = 2 * x2 - x1 + 1 / (N ** 2) * u[k]
-           # print(f"x2_new = 2* {x2} - {x1} + {u[k]} / {N}**2")
-            sum_u = sum_u + (u[k] ** 2)
-            x1 = x1_new
-            x2 = x2_new
-            # print('sum_u', sum_u)
-            # print(f"x1({k}) = {x1}")
-            # print(f"x2({k}) = {x2}", end="\n\n")
-        J = x1 - sum_u / (2 * _N)
+
+    for man in population:
         sum_u = 0
         x2 = 0
         x1 = 0
-        # print('J', J)
+        a = 0
+        b = 20
+
+        arr.clear()
+        u.clear()
+        man = man * values
+
+        ##################################################################################
+        # Odkodowanie chromosomu osobnika, w celu uzyskania zawartych w nim sterowań (u) #
+        ##################################################################################
+        for j in range(N):
+            arr.append(man[a:b])
+            a = a + 20
+            b = b + 20
+            u.append(np.sum(arr[j]))
+
+        #########################################################
+        # Obliczenie kolejnych stanów na bazie modelu stanowego #
+        #########################################################
+        for k in range(0, _N):
+            x1_new = x2
+            x2_new = 2 * x2 - x1 + 1 / (N ** 2) * u[k]
+            sum_u = sum_u + (u[k] ** 2)
+            x1 = x1_new
+            x2 = x2_new
+
+        ###########################################
+        # Obliczenie wskaźnika jakości sterowania #
+        ###########################################
+        J = x1 - sum_u / (2 * _N)
         all_fitness.append(J)
-    best_Js.append(max(all_fitness))
+
+    best_fitness_individuals.append(max(all_fitness))
     return all_fitness
 
 
-def sort_by_fitness(arr):
-    return arr[1]
+####################################################################
+# Funkcja pozwalająca na wybór opcji sortowania w funkcji sorted() #
+####################################################################
+def fitness_sort(array):
+    return array[1]
 
 
-def make_new_population(current_population):
+####################################################################################
+# Tworzenie nowej populacji poprzez mutacje oraz selekcje polegająca na odrzuceniu #
+# gorszej połowy osobników na podstawie wartości ich funkcji przystosowania        #
+####################################################################################
+def make_new_population(_current_population):
+    #################################################################
+    # Mutacja osobników z prawdopodobieństwem równym epsilon = 20%  #
+    # polegająca na zamianie wartości bitów na przeciwne            #
+    #################################################################
     mutated_population = []
-    # print('len current pop', len(current_population))
     mutated_population.clear()
-    # print(current_population[0])
-    for man in current_population:
+    for man in _current_population:
         new_man = []
         for bit in man:
             z = random.random()
@@ -82,66 +102,69 @@ def make_new_population(current_population):
         mutated_population.append(new_man)
 
     mutated_population = np.array(mutated_population)
-    # print('len mutated pop', len(mutated_population))
-    # print('mutants array', mutated_population)
 
-    whole_population = np.concatenate((current_population, mutated_population))
-    # print('whole_population', whole_population)
+    whole_population = np.concatenate((_current_population, mutated_population))
 
     fitness_array = fitness_function(N, whole_population)
-    # print('fitnessarray', fitness_array)
-    # print("size_fitnessarr", len(fitness_array))
-    # print('sorted', sorted(fitness_array))
-    zipped = zip(whole_population, fitness_array)
-    zipped_sorted = sorted(zipped, key=sort_by_fitness,
-                           reverse=True)  # sortujemy od najlepszych osobników żeby uciąć 100 najgorszych
-    bests = zipped_sorted[:len(zipped_sorted) // 2]  # tutaj obcinamy dolną połowę rozwiązań
-    # print('bests', bests)  # tutaj mamy array w formie array([0, 1, 0, 1 ..., 1, 0]), FITNESS), od najlepszego do najgorszego 100 osobników
-    new_p = [x[0] for x in bests]  # to wyżej to jest krok 7 , my możemy dodać przed tym metodę turniejową w której brać będziemy
-    new_p = np.array(new_p)  # dobierać osobników w 40 grupo po 5 osobników i segregować ich fitnessami w tych grupach, na tej postawie
-    # print('new_p', new_p)
 
+    ##########################################################################
+    # Sortujemy od najlepszych osobników, aby następnie usunąć gorszą połowę #
+    ##########################################################################
+    combined = zip(whole_population, fitness_array)
+    population_sorted = sorted(combined, key=fitness_sort, reverse=True)
+    better_half = population_sorted[:len(population_sorted) // 2]
+    new_p = [x[0] for x in better_half]  # Wyciągnięcie samej populacji z krotki zawierającej osobnika i jego wartość J
+    new_p = np.array(new_p)
     return new_p
 
 
+############################################
+# Ustawienie wartości parametrów symulacji #
+############################################
+our_solution = 0
 size_of_population = 70
-epsilon = 0.2
-iterations = 5000
-N = 5  # parameter from exercise
-# values = np.random.uniform(0, 1, 20*N)
+epsilon = 0.20
+iterations = 8741
+N = 5
 
 values = []
-best_Js = []
-# Set a length of the list to 10
+best_fitness_individuals = []
+
+#################################################
+# Wygenerowanie tablicy wartości do dekodowania #
+#################################################
 for i in range(0, 20 * N):
-    # any random float between 50.50 to 500.50
-    # don't use round() if you need number as it is
-    # v1 x = round(random.uniform(0, 0.1), 3)
     x = random.uniform(0, 0.1)
     values.append(x)
 
-# values = float("{:.3f}".format(values_x))
-# values = np.random.randint(0, 10, 20*N)
-
-# print('values', values)
-# print('len(values): ', len(values))
-
+###########################################################
+# Stworzenie populacji początkowej o określonej wielkości #
+###########################################################
 starting_population = np.random.randint(0, 2, (size_of_population, 20 * N))
 current_population = starting_population
-# print('current_population', current_population)
+
 start_all_t = time.time()
+
+##################################################
+# Pętla wykonująca algorytm określoną ilość razy #
+##################################################
 for i in range(iterations):
     start_t = time.time()
     new_population = make_new_population(current_population)
     duration = time.time() - start_t
-    #  print('new_population', new_population)
+    our_solution = max(best_fitness_individuals)
     current_population = new_population
-    print("\n" + bcolors.OKGREEN + f"==========================================\n\t\tITERATION NR:\t{i}\n==========================================\n" + bcolors.ENDC)
-    print('Processing time: {:.4f}'.format(duration))
-
 best = current_population[0]
+
 duration_all = time.time() - start_all_t
-print('Processing time of full program: {:.4f}'.format(duration_all))
-plt.plot(best_Js)
-# plt.figtext(0.5, 0.01, "one text and next text", ha="center", fontsize=6)
+
+################################################################
+# Wizualizacja najlepszego rozwiązania w kolejnych pokoleniach #
+################################################################
+print('Czas wykonywania się całego algorytmu: {:.4f} [s]'.format(duration_all))
+plt.plot(best_fitness_individuals)
+plt.title('Osiągnięta wartość funkcji J = {:.4f} po wykonaniu {:.1f} iteracji w czasie {:.3f} [s]'.format(our_solution,
+                                                                                                          iterations,
+                                                                                                          duration_all))
 plt.show()
+
